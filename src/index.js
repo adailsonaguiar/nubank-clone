f5f45f5413157ed1682238b06ca32abee93d1865
Adailson Aguiar <adailsonacj@live.com>
2019-11-01 21:21:47 -0300

    criar componentes

--- src/index.js
+++ src/index.js
@@ -4,6 +4,13 @@ import '~/config/ReactotronConfig';
 
 import Routes from '~/routes';
 
-const App = () => <Routes />;
+import {StatusBar} from 'react-native';
+
+const App = () => (
+  <>
+    <StatusBar barStyle="light-content" backgroundColor="#8B10AE" />
+    <Routes />
+  </>
+);
 
 export default App;

a01755ae5f63a0917e36bb5e09c08d38c430ee7d
Adailson Aguiar <adailsonacj@live.com>
2019-11-02 01:27:18 -0300

    alteração no header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,1 +1,22 @@
-    
+import React from 'react';
+import {View, Text} from 'react-native';
+
+import {Container, Top, Logo, Title} from './styles';
+import logo from '../../assets/Nubank_Logo.png';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+const Header = () => {
+  return (
+    <Container>
+      <Top>
+        <Logo source={logo} />
+        <Title>
+          <Text>Adailson</Text>
+        </Title>
+      </Top>
+      <Icon name="keyboard-arrow-down" size={20} color="#FFF" />
+    </Container>
+  );
+};
+
+export default Header;
--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import {Container} from './styles';
+import Header from '../../components/Header';
+import Tabs from '../../components/Tabs';
 
 import {View, Text} from 'react-native';
 
-export default function Main() {
-  return <Container />;
-}
+const Main = () => {
+  return (
+    <Container>
+      <Header />
+    </Container>
+  );
+};
+
+export default Main;

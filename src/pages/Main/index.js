dab147c4dc7a744660cd6070a6d53528b70c02f4
Adailson Aguiar <adailsonacj@live.com>
2019-11-10 17:30:18 -0300

    feat: arrastar card no eixo Y

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -4,6 +4,9 @@ import Header from '../../components/Header';
 import Tabs from '../../components/Tabs';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import Menu from '../../components/Menu';
+
+import {Animated} from 'react-native';
+import {PanGestureHandler, State} from 'react-native-gesture-handler';
 import {
   Card,
   CardHeader,
@@ -16,24 +19,52 @@ import {
 } from './styles';
 
 const Main = () => {
+  const translateY = new Animated.Value(0);
+
+  const animatedEvent = new Animated.event(
+    [
+      {
+        nativeEvent: {
+          translationY: translateY,
+        },
+      },
+    ],
+    {useNativeDriver: true},
+  );
+
+  const onHandlerStateChange = event => {};
+
   return (
     <Container>
       <Header />
       <Content>
         <Menu />
-        <Card>
-          <CardHeader>
-            <Icon name="attach-money" size={28} color="#666" />
-            <Icon name="visibility-off" size={28} color="#666" />
-          </CardHeader>
-          <CardContent>
-            <Title>Saldo Disponível</Title>
-            <Description>RS 543.298,43</Description>
-          </CardContent>
-          <CardFooter>
-            <Annotation>Transferência enviada no valor de R$ 132,90</Annotation>
-          </CardFooter>
-        </Card>
+        <PanGestureHandler
+          onGestureEvent={animatedEvent}
+          onHandlerStateChange={onHandlerStateChange}>
+          <Card
+            style={{
+              transform: [
+                {
+                  translateY: translateY,
+                },
+              ],
+            }}>
+            <CardHeader>
+              <Icon name="attach-money" size={28} color="#666" />
+              <Icon name="visibility-off" size={28} color="#666" />
+            </CardHeader>
+            <CardContent>
+              <Title>Saldo Disponível</Title>
+              <Description>RS 1.000.000,00</Description>
+            </CardContent>
+            <CardFooter>
+              <Annotation>
+                Transferência enviada no valor de R$ 132,90
+              </Annotation>
+            </CardFooter>
+          </Card>
+        </PanGestureHandler>
       </Content>
       <Tabs />
     </Container>
